39f3c47ed6badc5f516c0a141439ef91f65b75db
Clivaly Cardoso <clivalydev@gmail.com>
2022-08-19 07:08:08 -0300

    EKS_CLUSTER

--- infra/eks.tf
+++ infra/eks.tf
@@ -1,46 +1,21 @@
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
 
-  cluster_name                    = "my-cluster"
+  cluster_name                    = var.eks_cluster_name
   cluster_version                 = "1.21"
   cluster_endpoint_private_access = true
-  cluster_endpoint_public_access  = true
 
-  vpc_id     = "vpc-1234556abcdef"
-  subnet_ids = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghi345a"]
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
   
-  # EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
-    ami_type               = "AL2_x86_64"
-    disk_size              = 50
-    instance_types         = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
-    vpc_security_group_ids = [aws_security_group.additional.id]
-  }
-
   eks_managed_node_groups = {
-    blue = {}
-    green = {
+    green_cluster = {
       min_size     = 1
       max_size     = 10
-      desired_size = 1
-
-      instance_types = ["t3.large"]
-      capacity_type  = "SPOT"
-      labels = {
-        Environment = "test"
-        GithubRepo  = "terraform-aws-eks"
-        GithubOrg   = "terraform-aws-modules"
-      }
-      taints = {
-        dedicated = {
-          key    = "dedicated"
-          value  = "gpuGroup"
-          effect = "NO_SCHEDULE"
-        }
-      }
-      tags = {
-        ExtraTag = "example"
-      }
-    }
-  } 
+      desired_size = 3
+      vpc_security_group_ids = [aws_security_group.ssh_cluster.id]
+      instance_types = ["t2.micro"]
+      # capacity_type  = "SPOT"     
+    } 
+  }
 }
--- infra/sg.tf
+++ infra/sg.tf
@@ -1,4 +1,3 @@
-# SG public
 resource "aws_security_group" "ssh_cluster" {
   name        = "ssh_cluster"
   vpc_id      = module.vpc.vpc_id
@@ -20,28 +19,4 @@ resource "aws_security_group_rule" "ssh_clusterout" {
   protocol          = "-1"   # allowed all 
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.ssh_cluster.id
-}
-
-# SG private
-resource "aws_security_group" "alb_private" {
-  name        = "private_alb_ecs"
-  vpc_id      = module.vpc.vpc_id
-}
-
-resource "aws_security_group_rule" "tcp_alb_private_in" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"   # allowed all
-  source_security_group_id       = aws_security_group.alb_public.id
-  security_group_id = aws_security_group.alb_private.id
-}
-
-resource "aws_security_group_rule" "tcp_alb_private_out" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"   # allowed all 
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.alb_private.id
 }
